5088926623d7ced889d2562e3e2e0de6d3758945
codingJJJ <271533323@qq.com>
2021-11-06 18:59:19 +0800

    feat: 元组转对象

--- test.ts
+++ test.ts
@@ -1,3 +1,9 @@
-type MyPick<T, K extends keyof T> = { [P in K]: T[P] }
+const toutle = ["a", "b", "c"] as const;
 
-type MyReadonly<T> = { readonly [K in keyof T]: T[K] }
+type ToupleToObject<T extends readonly any[]> = {  [ K in T[number]]: T[K] } 
+
+const test: ToupleToObject<typeof toutle> = {
+  a: "a",
+  b: "b",
+  c: "c"
+}
