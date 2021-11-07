3936ffd9d92213af2fd865b9c08b8181ed001342
codingJJJ <271533323@qq.com>
2021-11-07 20:23:31 +0800

    feat: test

--- README.md
+++
@@ -1,5 +0,0 @@
-# type-challenges
-
-## ts训练笔记
-
-+ [github地址](https://github.com/type-challenges/type-challenges)
--- test.ts
+++ test.ts
@@ -1,9 +1,1 @@
-const toutle = ["a", "b", "c"] as const;
-
-type ToupleToObject<T extends readonly any[]> = {  [ K in T[number]]: T[K] } 
-
-const test: ToupleToObject<typeof toutle> = {
-  a: "a",
-  b: "b",
-  c: "c"
-}
+// test
