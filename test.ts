21d170d02ae0b311a1891a4a620d2f00fd4757f6
codingJJJ <271533323@qq.com>
2021-11-10 21:27:26 +0800

    feat: test

--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
-const toutle = ["a", "b", "c"] as const;
-
-type ToupleToObject<T extends readonly any[]> = {  [ K in T[number]]: T[K] } 
-
-const test: ToupleToObject<typeof toutle> = {
-  a: "a",
-  b: "b",
-  c: "c"
+function test(b: boolean) {
+  return b ? 1 : 2
 }
+
+
+
+
+
+const a: ReturnType<typeof test> = 
